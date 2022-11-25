5ed4300ea48cbc1eae13e90525ac6e02ee0955d0
Juraj Martinka <jumarko@gmail.com>
2022-11-25 11:50:19 +0100

    Generate project skeleon with tools.project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to net.curiousprogrammer/stats-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/net/curiousprogrammer/stats_clj_test.clj
@@ -0,0 +1,6 @@
+(ns net.curiousprogrammer.stats-clj-test
+  (:require [clojure.test :refer [deftest is testing]]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
